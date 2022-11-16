000bfb3753a7fca46729a38b7e78e84b3d82e257
Vladimir Baksheev <vladimir.baksheev@gmail.com>
2022-11-16 23:28:07 +0500

    Add 1 and 0 choices for main menu

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,10 +1,30 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
+
+    private static final int COMMAND_GREETINGS = 1;
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
+        printMenu();
 
-        Cli.greetingUser();
+        System.out.print("Your choice: ");
+        Scanner scanner = new Scanner(System.in);
+        int command = scanner.nextInt();
+        System.out.println();
+
+        if (command == COMMAND_GREETINGS) {
+            Cli.greetingUser(scanner);
+        }
+
+        scanner.close();
+    }
+
+    private static void printMenu() {
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
     }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -4,13 +4,9 @@ import java.util.Scanner;
 
 public class Cli {
 
-    public static void greetingUser() {
-        Scanner scanner = new Scanner(System.in);
-
+    public static void greetingUser(Scanner scanner) {
         System.out.print("May I have your name? ");
         String userName = scanner.next();
         System.out.println("Hello, " + userName + "!");
-
-        scanner.close();
     }
 }
